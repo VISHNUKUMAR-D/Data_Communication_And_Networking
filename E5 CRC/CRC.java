0196eb8b556aa1bc29594faa30288f84613766a8
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2021-11-10 18:21:53 +0530

    Update CRC.java

--- E5 CRC/CRC.java
+++ E5 CRC/CRC.java
@@ -1,6 +1,16 @@
-import java.util.*;
- 
+/*
+        Algorithm for CRC
+        1.Get the Data word from the user.
+        2.Get the generator polynomial from the user.
+        3.Concatenate the data word with (length of generator polynomial-1) zeros.
+        4.Do XOR operation on modified code and the generator polynomial.
+        5.Return the remainder and concatenate the code with the remainder.
+        6.Enter the received code and Repeat step 4 again and return remiander.
+        7.If the remainder is 1 then the Received code has an error.
+        8.If the remainder is 0 then the Received code has no error.
+*/
 
+import java.util.*;
 
  public class CRC{
      private static String Data_word="";
@@ -41,27 +51,23 @@ import java.util.*;
         int length = Generator.length();
         String one_zero = "01";
         code = data.substring(0, Generator.length());
-        if(code.charAt(0) == one_zero.charAt(1)){
-                process_XOR();
-        }
-        while(length < data.length()){
 
-                if(code.charAt(0) == one_zero.charAt(0)){
-                    code = code.substring(1, code.length());
-                    code += data.charAt(length++);
-                }
-                process_XOR();
+        while(length < data.length()){
+            if(code.charAt(0) == one_zero.charAt(0)){
+                code = code.substring(1, code.length());
+                code += data.charAt(length++);
+            }
+            process_XOR();    
         }
         return (code = code.substring(1, code.length()));
     }
+
     public static void process_XOR(){
         for(int i=0; i<Generator.length(); i++){
-                    if(code.charAt(i) == Generator.charAt(i)){
-                        remainder += "0";
-                    }
-                    else{
-                        remainder += "1";
-                    }
+            if(code.charAt(i) == Generator.charAt(i))
+                remainder += "0";                 
+            else
+                remainder += "1";
         }
         code = remainder;
         remainder = "";
