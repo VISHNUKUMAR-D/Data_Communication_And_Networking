2a9e94921f0ae2d029dabe8f97853eca7c503e81
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2022-01-03 12:51:32 +0530

    Update client.java

--- E1 SIMPLEX/client.java
+++ E1 SIMPLEX/client.java
@@ -1,16 +1,11 @@
 import java.io.*;
 import java.net.*;
 public class client{
-    public static void main(String args[]){
-        try{
+    public static void main(String args[]) throws Exception{
             Socket s = new Socket("localhost", 4999);
             DataInputStream in = new DataInputStream(s.getInputStream());
             String str = (String)in.readUTF();
             System.out.println(str);
             s.close();
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
     }
 }
