ac03c0c98fff3d4b873d87ee52f1266f7978942c
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2022-01-04 17:48:31 +0530

    Update Webpage_Client.java

--- E6 WEBPAGE_RETRIVING/Webpage_Client.java
+++ E6 WEBPAGE_RETRIVING/Webpage_Client.java
@@ -1,3 +1,4 @@
+
 /*
                     ALGORITHM FOR CLIENT
                     1.Creat Socket and pass hostname and port number as arguement 
@@ -12,22 +13,17 @@ import java.net.*;
 import java.util.*;
 
 public class Webpage_Client{
-    public static void main(String args[]){
-        try{
-            Socket Socket = new Socket("localhost", 3000);
-            DataInputStream input = new DataInputStream(Socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(Socket.getOutputStream());
+    public static void main(String args[]) throws Exception{
+            Socket s = new Socket("localhost", 3000);
+            DataInputStream input = new DataInputStream(s.getInputStream());
+            DataOutputStream output = new DataOutputStream(s.getOutputStream());
+            Scanner scan = new Scanner(System.in);
 
             System.out.print("ENTER THE URL OF A WEB-PAGE : ");
-            Scanner scan = new Scanner(System.in);
             String url = scan.nextLine();
             output.writeUTF(url);
-            String code =(String)input.readUTF();
+            String code = input.readUTF();
             System.out.println("THE GIVEN URL HAS THE FOLLOWING HTML CODE : \n"+code);
-            Socket.close();
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+            s.close();
     }
 }
