43acacab8edc853230a18425fee60343fdca9fb2
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2022-01-04 17:58:21 +0530

    Update DNS.java

--- E7 DNS/DNS.java
+++ E7 DNS/DNS.java
@@ -6,31 +6,24 @@
                             4.Print the IP address for the given HostName using getHostAddress() method
 */
 import java.net.*;
-import java.io.*;
 import java.util.*;
 
 public class DNS{
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
         Scanner scan = new Scanner(System.in);
         int n;
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         do{
             System.out.println("\nMENU:\n1.DNS\n2.EXIT");
             System.out.print("PLEASE, ENTER YOUR CHOICE : ");
             n = scan.nextInt(); 
             if(n==1)
             {
-                try {
                     System.out.print("\nENTER HOST NAME : ");
-                    String Host_Name = input.readLine();
-                    InetAddress address = InetAddress.getByName(Host_Name);
+                    String HostName = scan.next();
+                    InetAddress address = InetAddress.getByName(HostName);
                     System.out.println("\nHOST NAME : " + address.getHostName());
                     System.out.println("IP : " + address.getHostAddress());
-                } 
-                catch(IOException ioe) {
-                    ioe.printStackTrace();
-                }
             }
         }while(!(n==2));
     }
