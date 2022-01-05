2a747cfe246b793435be60b2b63b95cc719a3039
VISHNUKUMAR D <84860384+VISHNUKUMAR-D@users.noreply.github.com>
2022-01-05 11:42:33 +0530

    Update SW_Receiver.java

--- E9 SLIDE_WINDOW/SW_Receiver.java
+++ E9 SLIDE_WINDOW/SW_Receiver.java
@@ -13,28 +13,24 @@ import java.util.*;
 public class SW_Receiver{
     public static void main(String args[])throws Exception{
 
-            ServerSocket Server = new ServerSocket(1000);
+            ServerSocket Server = new ServerSocket(3000);
             Socket socket = Server.accept();
 
             DataInputStream input = new DataInputStream(socket.getInputStream());
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            int frame = Integer.parseInt((String)input.readUTF());
+            int frame = Integer.parseInt(input.readUTF());
             String Data[] = new String[frame];
 
             int i=0;
             while(i<frame){
-                int window = Integer.parseInt((String)input.readUTF());
-                int k=0;
-
+                int window = Integer.parseInt(input.readUTF());
                 for(int j=0; j<window; j++){
-                    Data[i] = (String)input.readUTF();
-                    System.out.println("THE Data IN "+k+" FRAME : "+Data[i]);
-                    i++;
-                    k++;
+                    Data[i] = input.readUTF();
+                    System.out.println("THE DATA IN "+j+" FRAME : "+Data[i]);
+                    output.writeUTF(Integer.toString(i++));
                 }
                 System.out.println("ACKNOWLEDGEMENT SENT FOR "+window+" FRAMES.");
-                output.writeUTF(Integer.toString(i));
             }
     }
 }
